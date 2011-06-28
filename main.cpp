aed098b3d5497776c998398d734972fe057b9574
Jacob Baines <baines.jacob@gmail.com>
2011-06-28 19:59:44 -0400

    Added a seed so that runs could be recreated.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <ctime>
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 #include <boost/cstdint.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/asio.hpp>
@@ -15,11 +16,25 @@ static char readBuffer[65356] = {0};
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if (argc < 2 || argc > 3)
     {
         std::cout << "You aren't doing it right." << std::endl;
         return 0;
     }
+    
+    std::string rootDirectory(argv[1]);
+    int seedValue = 0;
+    
+    if (argc == 3)
+    {
+        seedValue = boost::lexical_cast<int>(argv[2]);
+    }
+    else
+    {
+        seedValue = std::time(NULL);
+    }
+    
+    srand(seedValue);
     
     std::ifstream filestream;
     modifier::Randomizer packetModifier;    
@@ -30,8 +45,7 @@ int main(int argc, char* argv[])
     boost::asio::ip::tcp::endpoint server(boost::asio::ip::address::from_string("127.0.0.1"), 11111);
     outputSocket.connect(server);
 
-    boost::uint32_t timestamp = 0;
-    std::string rootDirectory(argv[1]);
+    //boost::uint32_t timestamp = 0;
     
     for (boost::filesystem::recursive_directory_iterator iter(rootDirectory);
          iter != boost::filesystem::recursive_directory_iterator(); ++iter)
@@ -89,10 +103,10 @@ int main(int argc, char* argv[])
                     break;
                 }
                                            
-                timestamp = std::time(NULL);
-                memcpy(readBuffer, &timestamp, sizeof(boost::uint32_t));
-                memcpy(readBuffer + sizeof(boost::uint32_t), "\0\0\0\0", 4);
-                boost::asio::write(outputSocket, boost::asio::buffer(readBuffer, 16));
+               // timestamp = std::time(NULL);
+               // memcpy(readBuffer, &timestamp, sizeof(boost::uint32_t));
+               // memcpy(readBuffer + sizeof(boost::uint32_t), "\0\0\0\0", 4);
+               // boost::asio::write(outputSocket, boost::asio::buffer(readBuffer, 16));
                 boost::uint32_t frameSize = *reinterpret_cast<boost::uint32_t*>(readBuffer + 12);
                         
                 filestream.read(readBuffer, frameSize);
@@ -109,6 +123,8 @@ int main(int argc, char* argv[])
     }
  
     outputSocket.close();   
+    
+    std::cout << "Your seed was: " << seedValue << std::endl;
     return 0;
 }
 
